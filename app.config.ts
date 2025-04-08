f44acd34cc341a40de3289b4f035e1e0c7fb6f9f
uktveris <marius.uktveris@gmail.com>
2025-04-08 22:13:16 +0300

    change p2p conn

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,65 @@ import dotenv from "dotenv/config";
 
 export default {
   expo: {
+    name: "speaksers-mobile",
+    slug: "speaksers-app",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/react-logo@2x.png",
+    scheme: "myapp",
+    userInterfaceStyle: "automatic",
+    splash: {
+      image: "./assets/images/react-logo@3x.png",
+      imageWidth: 300,
+      resizeMode: "cover",
+      backgroundColor: "#FF0000",
+    },
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.speaksers.speaksers-app",
+      infoPlist: {
+        UIBackgroundModes: ["audio"],
+        NSMicrophoneUsageDescription:
+          "This app uses microphone for audio calls.",
+      },
+    },
+    android: {
+      package: "com.speaksers.speaksers-app",
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      permissions: ["android.permission.RECORD_AUDIO"],
+    },
+    // web: {
+    //   bundler: "metro",
+    //   output: "static",
+    //   favicon: "./assets/images/favicon.png",
+    // },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/react-logo@3x.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff",
+        },
+      ],
+      "@config-plugins/react-native-webrtc",
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
     extra: {
+      router: {
+        origin: false,
+      },
+      eas: {
+        projectId: "e0729a28-a1b5-4a64-956b-e9058e95c178",
+      },
       SUPABASE_AUTH_URL: process.env.SUPABASE_AUTH_URL,
       SUPABASE_AUTH_API_KEY: process.env.SUPABASE_AUTH_API_KEY,
       GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
@@ -11,6 +69,18 @@ export default {
       SB_AUTH_TOKEN_NAME: process.env.SB_AUTH_TOKEN_NAME,
       STUN_SERVERS_URL: process.env.STUN_SERVERS_URL,
     },
-    scheme: "speaksers",
+    owner: "mariusuktveris",
   },
+  // expo: {
+  //   extra: {
+  //     SUPABASE_AUTH_URL: process.env.SUPABASE_AUTH_URL,
+  //     SUPABASE_AUTH_API_KEY: process.env.SUPABASE_AUTH_API_KEY,
+  //     GOOGLE_AUTH_CLIENT_ID: process.env.GOOGLE_AUTH_CLIENT_ID,
+  //     BACKEND_URL: process.env.BACKEND_URL,
+  //     BACKEND_URL_LOCAL: process.env.BACKEND_URL_LOCAL,
+  //     SB_AUTH_TOKEN_NAME: process.env.SB_AUTH_TOKEN_NAME,
+  //     STUN_SERVERS_URL: process.env.STUN_SERVERS_URL,
+  //   },
+  //   scheme: "speaksers",
+  // },
 };
--- app/(game)/dialogCall.tsx
+++ app/(game)/dialogCall.tsx
@@ -1,106 +1,181 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
 import { Audio } from "expo-av";
-import { MediaStream } from "react-native-webrtc";
+// import { MediaStream } from "react-native-webrtc";
 import { Pressable } from "react-native";
 import { Text } from "react-native";
 import socket from "@/server/socket";
-import { useLocalSearchParams } from "expo-router";
-import { Platform } from "react-native";
-import Constants from "expo-constants";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
 function DialogCall() {
   const { remoteSocketId, initCall } = useLocalSearchParams();
   console.log("remote socketId from params: " + remoteSocketId);
   console.log("initcall from params: " + initCall);
   const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [activeCall, setActiveCall] = useState(true);
+  const router = useRouter();
   const iceServers = [
-    // { urls: "stunserver.org" },
-    // { urls: "stun:stun4.l.google.com:5349" },
-    // { urls: "stun:stun.l.google.com:19302" },
+    { urls: "stun:stun.l.google.com:19302" },
     { urls: "STUN:freestun.net:3478" },
-    // { urls: "TURN:freestun.net:3478" },
   ];
-  const peerConn = new RTCPeerConnection({ iceServers: iceServers });
+  // const peerConn = new RTCPeerConnection({ iceServers: iceServers });
+  const peerConnRef = useRef<RTCPeerConnection | null>(null);
 
-  const onStreamUpdate = async (stream: MediaStream) => {
-    const { sound: newSound } = await Audio.Sound.createAsync(
-      { uri: stream.toURL() },
-      { shouldPlay: true },
-    );
-    setSound(newSound);
-  };
+  useEffect(() => {
+    peerConnRef.current = new RTCPeerConnection({ iceServers: iceServers });
+    socket.on("offer", async (data) => onOffer(data));
+    socket.on("answer", async (data) => onAnswer(data));
+    socket.on("ice-candidate", (data) => onIceCandidate(data));
+    socket.on("end-call", () => onEndCall());
 
-  const initPeerCall = async (remoteSocketId: string) => {
-    // peerConns.set(remoteSocketId, peerConn);
-    const offer = await peerConn.createOffer();
-    await peerConn.setLocalDescription(offer);
-    socket.emit("offer", { recipient: remoteSocketId, offer: offer });
-  };
+    peerConnRef.current.ontrack = (event) => {
+      const remoteStream = new MediaStream();
+      remoteStream.addTrack(event.track);
 
-  socket.on("offer", async (data) => {
-    console.log("offer received, proceeding");
-    // const peerConn = new RTCPeerConnection({ iceServers: iceServers });
-    // peerConns.set(data.from, peerConn);
-    console.log("connection created, proceeding");
-    peerConn.setRemoteDescription(new RTCSessionDescription(data.offer));
-    const answer = await peerConn.createAnswer();
-    await peerConn.setLocalDescription(answer);
-    console.log("answer ceated, proceeding");
-    socket.emit("answer", { recipient: remoteSocketId, answer: answer });
-  });
+      // Store or use this remoteStream to play audio
+      // For example, you can use Expo's `Audio.Sound.createAsync()` or similar
+    };
 
-  socket.on("answer", (data) => {
-    console.log("trying to add answer to desc");
-    // const peerConn = peerConns.get(remoteSocketId as string);
-    peerConn.setRemoteDescription(new RTCSessionDescription(data.answer));
-  });
-  socket.on("ice-candidate", async (data) => {
-    if (data.candidate) {
-      try {
-        console.log("incoming new ice candidate");
-        await peerConn.addIceCandidate(data.candidate);
-      } catch (error) {
-        console.log(
-          "error while adding ice candidate: " + (error as Error).message,
-        );
-      }
-    }
-  });
-  peerConn.addEventListener("icecandidate", (event) => {
-    if (event.candidate) {
-      console.log("Found ICE Candidate:", event.candidate);
-      socket.emit("ice-candidate", {
-        recipient: remoteSocketId,
-        candidate: event.candidate,
+    const initPeerCall = async (remoteSocketId: string) => {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
       });
+      stream.getTracks().forEach((track) => {
+        peerConnRef.current.addTrack(track, stream);
+      });
+      const offer = await peerConnRef.current.createOffer();
+      await peerConnRef.current.setLocalDescription(offer);
+      socket.emit("offer", { recipient: remoteSocketId, offer: offer });
+    };
+
+    const onOffer = async (data) => {
+      console.log("offer received, proceeding");
+      console.log("connection created, proceeding");
+      console.log("offer: ");
+      console.log(data.offer);
+      peerConnRef.current.setRemoteDescription(
+        new RTCSessionDescription(data.offer),
+      );
+      const answer = await peerConnRef.current.createAnswer();
+      await peerConnRef.current.setLocalDescription(answer);
+      console.log("answer ceated, proceeding");
+      socket.emit("answer", { recipient: remoteSocketId, answer: answer });
+    };
+
+    const onAnswer = async (data) => {
+      console.log("trying to add answer to desc: answer");
+      console.log(data.answer);
+      await peerConnRef.current.setRemoteDescription(
+        new RTCSessionDescription(data.answer),
+      );
+    };
+
+    const onIceCandidate = async (data) => {
+      if (data.candidate) {
+        try {
+          console.log("incoming new ice candidate");
+          // await peerConn.addIceCandidate(data.candidate);
+          await peerConnRef.current.addIceCandidate(
+            new RTCIceCandidate(data.candidate),
+          );
+        } catch (error) {
+          console.log(
+            "error while adding ice candidate: " + (error as Error).message,
+          );
+        }
+      }
+    };
+
+    peerConnRef.current.addEventListener("connectionstatechange", (event) => {
+      if (peerConnRef.current.connectionState === "connected") {
+        console.log("connection established!!");
+      }
+    });
+
+    if (initCall === "true") {
+      initPeerCall(remoteSocketId as string);
     }
+
+    peerConnRef.current.addEventListener("icecandidate", (event) => {
+      if (event.candidate) {
+        console.log("Found ICE Candidate:", event.candidate);
+        socket.emit("ice-candidate", {
+          recipient: remoteSocketId,
+          candidate: event.candidate,
+        });
+      }
+    });
+
+    return () => {
+      socket.off("offer");
+      socket.off("answer");
+      socket.off("ice-candidate");
+      socket.off("end-call");
+      peerConnRef.current?.close();
+    };
   });
-  peerConn.addEventListener("connectionstatechange", (event) => {
-    if (peerConn.connectionState === "connected") {
-      console.log("connection established!!");
-    }
-  });
-  // socket.on("ice-candidate", async (data) => {
-  //   console.log("ice candidate received");
-  //   await addCandidate(peerConns.get(data.from)!, data.candidate);
+
+  // socket.on("offer", async (data) => {
+  //   console.log("offer received, proceeding");
+  //   console.log("connection created, proceeding");
+  //   console.log("offer: ");
+  //   console.log(data.offer);
+  //   peerConn.setRemoteDescription(new RTCSessionDescription(data.offer));
+  //   const answer = await peerConn.createAnswer();
+  //   await peerConn.setLocalDescription(answer);
+  //   console.log("answer ceated, proceeding");
+  //   socket.emit("answer", { recipient: remoteSocketId, answer: answer });
   // });
 
-  // if (initCall === "true") {
-  //   console.log("initiating call");
-  //   initPeerCall(remoteSocketId as string, onStreamUpdate);
-  // } else {
-  //   const peerConn = createPeerConn(remoteSocketId as string, onStreamUpdate);
-  // }
-  if (initCall === "true") {
-    initPeerCall(remoteSocketId as string);
-  }
-  return (
+  // socket.on("answer", async (data) => {
+  //   console.log("trying to add answer to desc: answer");
+  //   console.log(data.answer);
+  //   await peerConn.setRemoteDescription(new RTCSessionDescription(data.answer));
+  // });
+
+  // socket.on("ice-candidate", async (data) => {
+  //   if (data.candidate) {
+  //     try {
+  //       console.log("incoming new ice candidate");
+  //       // await peerConn.addIceCandidate(data.candidate);
+  //       await peerConn.addIceCandidate(new RTCIceCandidate(data.candidate));
+  //     } catch (error) {
+  //       console.log(
+  //         "error while adding ice candidate: " + (error as Error).message,
+  //       );
+  //     }
+  //   }
+  // });
+
+  // socket.on("end-call", () => {
+  //   setActiveCall(false);
+  // });
+  const handleGoBack = () => {
+    router.navigate("/");
+  };
+
+  const onEndCall = () => {
+    setActiveCall(false);
+  };
+
+  const handleEndCall = () => {
+    socket.emit("end-call", { recipient: remoteSocketId });
+    setActiveCall(false);
+  };
+
+  return activeCall ? (
     <View>
-      {/* <Pressable onPress={}>
-        <Text>start call</Text>
-      </Pressable> */}
       <Text>this is dialog call screen</Text>
+      <Pressable onPress={handleEndCall}>
+        <Text>end call</Text>
+      </Pressable>
+    </View>
+  ) : (
+    <View>
+      <Pressable onPress={handleGoBack}>
+        <Text>call ended, go back</Text>
+      </Pressable>
     </View>
   );
 }
