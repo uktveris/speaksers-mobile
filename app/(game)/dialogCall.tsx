defa06ec9c668ecda5fec02c5a7dd16fb805171e
uktveris <marius.uktveris@gmail.com>
2025-04-09 17:22:02 +0300

    improve p2p

--- app/(game)/dialogCall.tsx
+++ app/(game)/dialogCall.tsx
@@ -1,18 +1,21 @@
 import { useEffect, useRef, useState } from "react";
 import { View } from "react-native";
-import { Audio } from "expo-av";
-// import { MediaStream } from "react-native-webrtc";
+// import { RTCView, MediaStream } from "react-native-webrtc";
 import { Pressable } from "react-native";
 import { Text } from "react-native";
 import socket from "@/server/socket";
 import { useLocalSearchParams, useRouter } from "expo-router";
+import { Platform } from "react-native";
 
 function DialogCall() {
   const { remoteSocketId, initCall } = useLocalSearchParams();
   console.log("remote socketId from params: " + remoteSocketId);
   console.log("initcall from params: " + initCall);
-  const [sound, setSound] = useState<Audio.Sound | null>(null);
+  const [remoteStream, setRemoteStream] = useState<MediaStream | null>(null);
   const [activeCall, setActiveCall] = useState(true);
+  // web testing
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isWeb, setIsWeb] = useState(false);
   const router = useRouter();
   const iceServers = [
     { urls: "stun:stun.l.google.com:19302" },
@@ -20,6 +23,20 @@ function DialogCall() {
   ];
   // const peerConn = new RTCPeerConnection({ iceServers: iceServers });
   const peerConnRef = useRef<RTCPeerConnection | null>(null);
+
+  // TODO: on production remove this effect
+  // TODO: stream needs to be played on mobile speakers
+  useEffect(() => {
+    if (Platform.OS === "web") setIsWeb(true);
+    if (audioRef.current && remoteStream) {
+      audioRef.current.srcObject = remoteStream;
+      audioRef.current
+        .play()
+        .catch((error) =>
+          console.log("error playing audio stream: " + error.message),
+        );
+    }
+  }, [remoteStream]);
 
   useEffect(() => {
     peerConnRef.current = new RTCPeerConnection({ iceServers: iceServers });
@@ -29,11 +46,10 @@ function DialogCall() {
     socket.on("end-call", () => onEndCall());
 
     peerConnRef.current.ontrack = (event) => {
-      const remoteStream = new MediaStream();
-      remoteStream.addTrack(event.track);
-
-      // Store or use this remoteStream to play audio
-      // For example, you can use Expo's `Audio.Sound.createAsync()` or similar
+      const stream = event.streams[0] as unknown as MediaStream;
+      setRemoteStream(stream);
+      setRemoteStream(event.streams[0]);
+      console.log("set remote stream: " + event.streams[0]);
     };
 
     const initPeerCall = async (remoteSocketId: string) => {
@@ -41,6 +57,7 @@ function DialogCall() {
         audio: true,
         video: false,
       });
+      // TODO: on production change to mobile speakers
       stream.getTracks().forEach((track) => {
         peerConnRef.current.addTrack(track, stream);
       });
@@ -114,58 +131,45 @@ function DialogCall() {
       socket.off("end-call");
       peerConnRef.current?.close();
     };
-  });
+  }, []);
 
-  // socket.on("offer", async (data) => {
-  //   console.log("offer received, proceeding");
-  //   console.log("connection created, proceeding");
-  //   console.log("offer: ");
-  //   console.log(data.offer);
-  //   peerConn.setRemoteDescription(new RTCSessionDescription(data.offer));
-  //   const answer = await peerConn.createAnswer();
-  //   await peerConn.setLocalDescription(answer);
-  //   console.log("answer ceated, proceeding");
-  //   socket.emit("answer", { recipient: remoteSocketId, answer: answer });
-  // });
-
-  // socket.on("answer", async (data) => {
-  //   console.log("trying to add answer to desc: answer");
-  //   console.log(data.answer);
-  //   await peerConn.setRemoteDescription(new RTCSessionDescription(data.answer));
-  // });
-
-  // socket.on("ice-candidate", async (data) => {
-  //   if (data.candidate) {
-  //     try {
-  //       console.log("incoming new ice candidate");
-  //       // await peerConn.addIceCandidate(data.candidate);
-  //       await peerConn.addIceCandidate(new RTCIceCandidate(data.candidate));
-  //     } catch (error) {
-  //       console.log(
-  //         "error while adding ice candidate: " + (error as Error).message,
-  //       );
-  //     }
-  //   }
-  // });
-
-  // socket.on("end-call", () => {
-  //   setActiveCall(false);
-  // });
   const handleGoBack = () => {
     router.navigate("/");
   };
 
   const onEndCall = () => {
+    console.log("ending the call");
     setActiveCall(false);
+    if (peerConnRef.current) {
+      peerConnRef.current.close();
+      peerConnRef.current = null;
+    }
+    if (remoteStream) {
+      remoteStream.getTracks().forEach((track) => track.stop());
+      setRemoteStream(null);
+    }
   };
 
   const handleEndCall = () => {
     socket.emit("end-call", { recipient: remoteSocketId });
-    setActiveCall(false);
+    onEndCall();
   };
+
+  const webAudio = (
+    <div>
+      <h2>Remote Audio</h2>
+      <audio ref={audioRef} autoPlay controls />
+    </div>
+  );
 
   return activeCall ? (
     <View>
+      {/* remoteStream ? ( */}
+      {/* <RTCView
+          streamURL={remoteStream.toURL()}
+          style={{ width: 0, height: 0 }}
+        /> */}
+      {isWeb && webAudio}
       <Text>this is dialog call screen</Text>
       <Pressable onPress={handleEndCall}>
         <Text>end call</Text>
@@ -173,11 +177,18 @@ function DialogCall() {
     </View>
   ) : (
     <View>
+      <Text>this is dialog call screen</Text>
+      <Pressable onPress={handleEndCall}>
+        <Text>end call</Text>
+      </Pressable>
       <Pressable onPress={handleGoBack}>
         <Text>call ended, go back</Text>
       </Pressable>
     </View>
   );
+  // : (
+  //   <View></View>
+  // );
 }
 
 export default DialogCall;
